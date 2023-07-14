e220aab189076dcb6ad0c54ba82775ba56289d07
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-14 12:49:55 -0700

    added task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - runs main code
+*Return: returns 0
+*/
+
+int main(void)
+{
+	for (char A = 'a'; A <= 'Z'; A++)
+		putchar(A);
+	putchar(\n);
+
+	return (0);
+}
