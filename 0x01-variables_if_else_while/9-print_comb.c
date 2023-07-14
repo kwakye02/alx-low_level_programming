60b6574867e26c1a23d574136372453f13fa83c5
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-14 14:34:38 -0700

    Added task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,11 @@ int main(void)
 	int a;
 
 	for (a = 48; a < 58; a++)
+	{
 		putchar(a);
-
-	for (a = 97; a < 103; a++)
-		putchar(a);
-
+		if (a < 57)
+			putchar(',');
+	}
 	putchar('\n');
 
 	return (0);
