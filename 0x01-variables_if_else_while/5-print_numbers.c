f796500302acd5377f20b5c61d1f22d1bb5a75d4
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-14 13:47:56 -0700

    added task 5 and 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - runs main code
+* Return: returns a success value
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - runs main code
+* Return - returns the value 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
