2a0d47142ee19bceeddadeaeede7fc9265ab49b7
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-18 13:29:56 -0700

    added task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_sign - prints the sign of a number
+*@n: number to check
+*Return: returns 1 or 0
+*/
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 48)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+		_putchar(45);
+		return (-1);
+
+}
