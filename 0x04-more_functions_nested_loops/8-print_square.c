96e19087cbd9927f61d3e35696f6d540aff61675
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-21 06:07:22 -0700

    added task 9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_square - prints a square, followed by a new line
+  * @size: size of the square
+  */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
