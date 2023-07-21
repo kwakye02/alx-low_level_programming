0121762c77839e9808a61cee1caa1493690989aa
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-21 06:20:53 -0700

    added task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,22 +14,18 @@ int main(void)
 		if ((n % 3) == 0 && (n % 5) == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if ((n % 3) == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if ((n % 5) == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", n);
-			printf(" ");
 		}
 	}
 
