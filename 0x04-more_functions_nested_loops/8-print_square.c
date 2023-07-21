6a74add38a4c802783e2305bd4a06b1f3c6e8aa6
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-21 06:29:04 -0700

    updated task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-  * print_square - prints a square, followed by a new line
-  * @size: size of the square
+*print_square - prints a square, followed by a new line
+*@size: size of the square
+*Return: returns 0
   */
 void print_square(int size)
 {
