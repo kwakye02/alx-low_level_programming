3b8a81a8360a8c8f386a930040d36c261f6d365a
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-24 16:16:09 -0700

    added task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- * Return: returns 0
+* *_strcpy - copies the string pointed to by 'src' to
+* the buffer pointed to by 'dest'
+* @dest: char type string
+* @src: char type string
+* Return: return pointer to dest
  */
-int main(void)
-{
-    char s1[98];
-    char *ptr;
 
-    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", ptr);
-    return (0);
-}
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+
