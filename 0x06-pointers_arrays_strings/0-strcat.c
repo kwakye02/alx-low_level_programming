94ebdef6ab349c3441d9ec9a7c25db0ea56223c6
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-27 12:03:13 -0700

    added task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,15 @@ char *_strcat(char *dest, char *src)
 	int i = 0, len = 0;
 
 	while (dest[i] != '\0')
+	{
 		len++;
 		i++;
+	}
 	i = 0;
 	while (src[i] != '\0')
+	{
 		dest[len + i] = src[i];
 		i++;
+	}
 	return (dest);
 }
