cc33f8be20245f5be372ace2dfec9232eaa2bd63
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-28 10:32:41 -0700

    added tasks

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,12 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int a;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	if (i > j)
-		return (15);
-	else if (i < j)
-		return (-15);
-	else
-		return (0);
+	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+			return (s1[a] - s2[a]);
+	}
+	return (0);
 }
