ac163d282980d456c9dfa999ed5fa2b8fcf89319
kwakye02 <kwakyegeorge0@gmail.com>
2023-07-28 13:27:38 -0700

    added task 100 and 101

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,16 +10,16 @@ char *rot13(char *str)
 	int i;
 	int j;
 
-	char rot1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char rot13[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot14[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; rot1[j] != '\0'; j++)
+		for (j = 0; rot13[j] != '\0'; j++)
 		{
-			if (str[i] == rot1[j])
+			if (str[i] == rot13[j])
 			{
-				str[i] = rot2[b];
+				str[i] = rot14[j];
 				break;
 			}
 		}
