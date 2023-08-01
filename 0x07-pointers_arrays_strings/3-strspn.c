9a766667eb8d0e17f6757c934be699c406dc3856
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-01 15:17:30 -0700

    added task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stddef.h>
+/**
+* _strspn -  gets the length of a prefix substring
+* @s: main string pointer
+* @accept: special string pointer 
+* @Return: return len
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0, i = 0, j = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				n = n + 1;
+				break;
+			}
+			j++
+		}
+
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+		i++
+
+	}
+	return (n);
+}
