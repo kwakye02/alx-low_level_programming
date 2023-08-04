3903584c00eda6dae8fe7b21985b9219fd28cd17
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-04 07:08:46 -0700

    added task 0

---
+++ 0-whatsmyname.c/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - runs main code
+*@argc: counts number of arguments
+*@argv: stores the arguments
+*Return: returns 0
+*/
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	printf("%s \n", *argv);
+
+	return (0);
+}
---
+++ 0-whatsmyname.c/README.md
@@ -0,0 +1,1 @@
+Tutorials on argv and argc
