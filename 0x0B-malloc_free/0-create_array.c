03bf97e56f73eac83169090b447c435be37896df
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-08 16:12:26 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,14 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	ptr = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
-	}
-
-	if (size == 0)
-	{
-		return (NULL);
 	}
 	return (ptr);
 }
