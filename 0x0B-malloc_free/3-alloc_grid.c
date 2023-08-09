44f3c4de5498a77da5b99d1e0060c681556f3561
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-09 05:19:14 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,6 +21,11 @@ int **alloc_grid(int width, int height)
 		arr[i] = (int *)malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
+			for (j = 0; j < width; j++)
+			{
+				free(arr[j];
+			}
+			free(arr)
 			return (NULL);
 		}
 	}
