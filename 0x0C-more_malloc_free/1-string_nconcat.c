967dd8d37d9b8feb4adef926cf679e00b1ff09b7
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-11 12:23:17 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; s2[j] != '\0'; j++)
 		len2++;
 
-	if ( n >= len2)
+	if (n >= len2)
 		n = len2;
 	ptr = malloc((n + len1 + 1) * sizeof(char));
 
