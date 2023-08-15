eba89aee843356c2fe68d46c1f8224c3fb2e49f7
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-15 09:50:37 -0700

    task 2

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-
 /**
  * main - check the code
  *
@@ -11,7 +10,7 @@ int main(void)
     struct dog my_dog;
 
     my_dog.name = "Poppy";
-    my_dog.age = 3.5;
+/*    my_dog.age =*/
     my_dog.owner = "Bob";
     print_dog(&my_dog);
     return (0);
