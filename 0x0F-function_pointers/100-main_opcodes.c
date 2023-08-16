10022250f2d816f81d88046397847d54e82ac167
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-16 13:44:53 -0700

    task 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints opcodes
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, num;
+	char *ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+
+	if (num < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	ptr = (char *)main;
+
+	for (i = 0; i < num; i++)
+	{
+		if (i == num - 1)
+		{
+			printf("%02hhx\n", ptr[x]);
+			break;
+		}
+		printf("%02hhx ", ptr[x]);
+	}
+	return (0);
+}
