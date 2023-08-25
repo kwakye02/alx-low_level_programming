050e951c5c0259db5665be57c2fdb69c2a24b289
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-24 17:42:56 -0700

    added task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*list_len -  returns the number of elements in a linked list_t list
+*@h: the list being used
+*Return: node count
+*/
+size_t list_len(const list_t *h)
+{
+	const list_t *new_node = h;
+	size_t count = 0;
+
+	while (new_node != NULL)
+	{
+		new_node = new_node->next;
+		count++;
+	}
+	return (count);
+}
