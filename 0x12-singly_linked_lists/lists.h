a6faff42840d2bf246586be7157ed40e12b12cf8
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-24 17:32:08 -0700

    added task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,9 +6,9 @@
 #include <string.h>
 
 /**
-*struct my_list - represents node in a singly linked list
-*@data: dynamically allocated string to be used
-*@length: length of node
+*struct list_s - represents node in a singly linked list
+*@str: dynamically allocated string to be used
+*@len: length of node
 *@next: pointer to next node
 */
 typedef struct list_s
@@ -16,8 +16,9 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+
 #endif
