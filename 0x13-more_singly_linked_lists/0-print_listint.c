50af7a6edf8b0d711feadd4efe948f12092da34b
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-29 09:25:20 -0700

    added task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+*print_listint - prints all elements of a list
+*@h - list being used
+*@Return: the number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp;
+	size_t i = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*struct list_s - represents node in a singly linked list
+*@n: length of node
+*@next: pointer to next node
+*/
+typedef struct list_s
+{
+	int n;
+	struct list_s *next;
+} listint_t;
+
+/*prototypes*/
+size_t print_listint(const listint_t *h);
+
+#endif
