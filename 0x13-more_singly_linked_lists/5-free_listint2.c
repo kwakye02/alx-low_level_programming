73a8a12763e0a1c6198ecdd97670f5733014cfa6
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-29 10:48:21 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*free_listint2 - frees list
+*@head: pointer to the list
+*Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *current_node;
+
+	current_node = *head;
+	while (current_node != NULL)
+	{
+		temp = current_node->next;
+		free(current_node);
+		current_node = temp;
+	}
+
+}
