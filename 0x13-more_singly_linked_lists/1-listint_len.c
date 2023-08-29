a38c432f90b15ca0d973d564f75f449c3671bd72
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-29 09:40:33 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*listint_len - returns the length of a list
+*@h: list to be used
+*Return: length of list
+*/
+size_t listint_len(const listint_t *h)
+{
+	const lisint_t *temp;
+	size_t count = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,5 +18,6 @@ typedef struct list_s
 
 /*prototypes*/
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
