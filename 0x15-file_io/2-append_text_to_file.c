fbeacd971fbea974fce5c58bd0e89d4ffed72c4b
kwakye02 <kwakyegeorge0@gmail.com>
2023-09-08 16:28:52 -0700

    added task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
 *@filename: name of the file
 *Return: 1 or -1
 */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t bw;
 	int fd;
